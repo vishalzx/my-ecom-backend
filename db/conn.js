1679c76e9875f42fdd654263eb8502a602a4fdeb
vishalzx <108291592+vishalzx@users.noreply.github.com>
2025-07-05 21:31:10 +0530

    Initial commit: backend

---
+++ db/conn.js
@@ -0,0 +1,10 @@
+const mongoose= require("mongoose");
+
+// Database connection with mongodb
+mongoose.connect("mongodb+srv://vishalgupta11zx:aarvidb@cluster0.evqqsqa.mongodb.net/e-commerce")
+.then(()=>{
+    console.log("connection is successful")
+}).catch((err)=>{
+    console.log(err);
+    console.log("No connection");
+})
